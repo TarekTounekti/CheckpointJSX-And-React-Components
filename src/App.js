4fe98e8fc1e49aaa196bb4d9708bc491b7cb097e
TarekTounekti <tarektounekti1993@gmail.com>
2023-10-01 21:28:08 +0100

    CheckpointJSX-And-React-Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Name from './Name';
+import Price from './Price';
+import Description from './Description';
+import product from './product';
+import { Card } from 'react-bootstrap';
 
-function App() {
+// Define the main App component
+const App = () => {
+  const firstName = 'Tarek Tounekti'; // Define a firstName variable
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card>
+        <Card.Img variant="top" src={product.image} alt="Product" /> {/* Display the product image */}
+        <Card.Body>
+          <Card.Title>
+            <Name name={product.name} /> {/* Render the Name component with the product name */}
+          </Card.Title>
+          <Card.Text>
+            <Price price={product.price} /> {/* Render the Price component with the product price */}
+            <Description description={product.description} /> {/* Render the Description component with the product description */}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <p>
+        Hello, {firstName ? firstName : 'there'}! {/* Display a greeting message with the firstName variable */}
+      </p>
     </div>
   );
-}
+};
 
-export default App;
+export default App; // Export the App component as the default export
